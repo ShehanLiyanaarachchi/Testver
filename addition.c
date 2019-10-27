2856ad15e21c2d520fc2ef1234559869427f6d33
ShehanLiyanaarachchi <Shehanliyanaarachchi111@gmail.com>
2019-10-27 08:28:55 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+
+{
+    int no1,no2,total;
+
+    printf("Input first number ");
+    scanf("%d",&no1);
+    printf("Input second number ");
+    scanf("%d",&no2);
+    total=(no1+no2);
+    printf("The total is %d \n",total);
+
+
+}
